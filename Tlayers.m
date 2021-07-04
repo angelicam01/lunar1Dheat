8d6a4545d4df764de62e05d1978dabba6d343771
angelicam01 <86896325+angelicam01@users.noreply.github.com>
2021-07-03 17:36:58 -0700

    Delete Tlayers.m

--- Tlayers.m
+++
@@ -1,7 +0,0 @@
-function Tnew = Tlayers(T,dt,rho,cp,K,g1,g2)
-
-alpha            = g1.*K(1:end-2);
-beta             = g2.*K(2:end-1);
-Tnew             = T(2:end-1) + (dt ./ (rho(2:end-1) .*cp(2:end-1))).* (alpha.*T(1:end-2) - ((alpha + beta).* T(2:end-1)) + (beta.* T(3:end)));    
-
-end
